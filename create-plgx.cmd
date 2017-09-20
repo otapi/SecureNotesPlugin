4afb0b80a05e54a252809c547d45c5684537fc30
otapi0 <otapi0@gmail.com>
2017-09-21 00:32:22 +0200

    r

--- create-plgx.cmd
+++ create-plgx.cmd
@@ -1,8 +1,14 @@
+@ECHO OFF
 ECHO Creating KeePass plugin package
-set PLUGIN_NAME=KeeOneDriveSync
-rd /s /q %~dp0\obj %~dp0\bin
-del %~dp0%PLUGIN_NAME%.plgx
-"C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %~dp0
+set PLUGIN_NAME=SecureNotesPlugin
+SET PP=%~dp0
+SET PARENT_PATH=%PP:~0,-1%
+cd %~dp0
+rd /s /q %~dp0\obj %~dp0\bin 2>nul
+del ..\%PLUGIN_NAME%.plgx 2>nul
+del "C:\Program Files (x86)\KeePass Password Safe 2\plugins\%PLUGIN_NAME%.plgx" 2>nul
+"C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %PARENT_PATH%
+move ..\%PLUGIN_NAME%.plgx "C:\Program Files (x86)\KeePass Password Safe 2\plugins\%PLUGIN_NAME%.plgx"
+ECHO KeePass Plugin package has been created and copied to plugins, let's start KeePass
 PAUSE
-'ren %~dp0%SourceFolder%.plgx %PLUGIN_NAME%.plgx
-ECHO KeePass Plugin package has been created
+
